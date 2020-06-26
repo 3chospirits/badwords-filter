feefb719931926e281be66a89379776ed1a26901
3chospirits <3chospirits@gmail.com>
2020-06-26 01:05:37 -0400

    pre-release

--- index.js
+++ index.js
@@ -1,42 +1,140 @@
 class Filter {
+	/**
+	 * Constructs filter 
+	 * @param {Array} list option to insert your own array of bad words
+	 * @param config options for the filter
+	 * @param {String} config.cleanWith a character to replace bad words with [default: '*']
+	 * @param {number} config.strictness 0: high, 1: medium, 2:low, 3: off [default: 1]
+	 * @param {boolean} config.useRegex true for enabling regex filtering, false for exact dictionary match *WARNING: large amounts of regex is much slower* [defailt: false]
+	 */
+	constructor(list, config) {
+		if (!list) list = require("./filtersets/en.json").filter;
+		this.userRegex = config ? config.userRegex : false;
+		if (this.userRegex) {
+			this.filter = new Set(
+				list.map((r) => {
+					new RegExp(r, "g");
+				})
+			);
+		} else {
+			this.filter = new Set(list);
+		}
+
+		this.config = {};
+		this.cleanWith = config && config.cleanWith ? config.cleanWith : "*";
+		this.minFiltered = this.userRegex ? 0 : this.filter.values().next().value.length; //ADD DEFAULT VALUE FOR DEFAULT LIST
+		if (!this.userRegex)
+			this.filter.forEach((e) => {
+				if (e.length < this.minFiltered) this.minFiltered = e.length;
+			});
+
+		this.strictness = config && config.strictness;
+		this.replacements = new Map([
+			["!", "i"],
+			["@", "a"],
+			["$", "s"],
+			["3", "e"],
+			["8", "b"],
+			["1", "i"],
+			["0", "o"],
+			["4", "h"],
+			["7", "t"],
+			["9", "g"],
+			["6", "b"],
+			["8", "b"],
+		]);
+	}
+	/**
+	 * converts to lowercase, replaces accented characters, replaces common symbol/l33t text, removes non-alphabetical characters
+	 * @param {String} string string to normalize
+     * @returns {String} cleaned string
+	 */
+	normalize(string) {
+		string = string
+			.toLowerCase()
+			.normalize("NFD")
+			.replace(/[\u0300-\u036f]/g, ""); //removes accented characters
+		this.replacements.forEach((replWith, targ) => (string = string.replace(new RegExp(targ, "g"), replWith)));
+		return string.replace(/[^a-zA-Z\s]/g, ""); //removes non-alphabetical characters
+	}
     /**
-     * 
-     * @param {Array} list option to insert your own array of bad words
-     * @param config options for the filter
-     * @param {String} config.cleanWith a character to replace bad words with
-     * @param {number} config.strictness 0: high, 1: medium, 2:low, 3: off 
+     * censors filtered words
+     * @param {String} string message to censor filter words 
+     * @returns {String} cleaned up message with filter words censored by cleanWith string
      */
-    constructor(list, config){
-        this.filter = new Set(!list?require("./filtersets/en.json").filter:list);
-        this.config = {};
-        this.config.cleanWith = (config && config.cleanWith)?config.cleanWith:"*";
-        this.minFiltered = this.filter.values().next().value.length; //ADD DEFAULT VALUE FOR DEFAULT LIST
-        this.filter.forEach(e=>{
-            if (e.length < this.minFiltered) this.minFiltered = e.length;
-        })
-        this.strictness = (config && config.strictness)
-        //console.log(this.minFiltered);
+	clean(string) {
+        let censorIndexes = new Set(this.getUncleanWordIndexes(string));
+        return string.split(/ +/g).map((w, i) => {
+            if (censorIndexes.has(i)) return this.cleanWith.repeat(w.length);
+            return w;
+        }).join(" ");
     }
-
-    clean(string){
-
+	getAllCombos(string) {
+		return string.split(/ +/g).map((w) => {
+			if (/(.)\1{1,}/.test(w) && w.length > this.minFiltered)
+				//only tests those with at least one double char
+				return allPossibleCases(combos(charSplitter(w)));
+			return [w];
+		});
+	}
+	debug() {
+		console.log(this.filter);
+		console.log(this.filter.has("KKK"));
     }
-    debug(string){
-        return splitIntoCombos(string);
+    /**
+     * gets all the indexes of words that are filtered
+     * @param {String} string message to check
+     * @returns {Array} indexes of filtered words, empty if none detected
+     */
+    getUncleanWordIndexes(string){
+        let uncleanIndexes = [];
+        let arr = this.getAllCombos(this.normalize(string));
+		for (let i = 0; i < arr.length; i++) 
+			for (let j = 0; j < arr[i].length; j++) 
+				if (this.isWordUnclean(arr[i][j])) uncleanIndexes.push(i);
+		return uncleanIndexes;
     }
+    /**
+     * checks if a string has any filtered words
+     * @param {String} string message to test 
+     * @returns {boolean} true if contains filtered words
+     */
+	isUnclean(string) {
+		let arr = this.getAllCombos(this.normalize(string));
+		for (let i = 0; i < arr.length; i++) 
+			for (let j = 0; j < arr[i].length; j++) 
+				if (this.isWordUnclean(arr[i][j])) return true;
+		return false;
+    }
+    /**
+     * Checks if a word is filtered or not
+     * @param {String} word word to check
+     * @returns {boolean} Returns true if is filtered word
+     */
+	isWordUnclean(word) {
+		if (this.userRegex) {
+            let unclean = false;
+			this.filter.forEach((r) => {
+				if (r.test(word)) unclean = true;
+            });
+            return unclean;
+        }
+        else
+            return this.filter.has(word);
+	}
 }
 module.exports = Filter;
 
-function splitIntoCombos(string){
-    //return console.log(string.split(/ +/g))//.map(w=>console.log(w + "\n"));
-    return string.split(/ +/g).map(w=>{
-        if (/(.)\1{1,}/.test(w) && w.length > this.maxFiltered) //only tests those with at least one double char
-            return allPossibleCases(combos(charSplitter(w)));
-        return w;
-    });
-}
-function comboWord(word){
-    return allPossibleCases(combos(charSplitter(word)))
+function splitIntoCombos(string) {
+	//return console.log(string.split(/ +/g))//.map(w=>console.log(w + "\n"));
+	return string.split(/ +/g).map((w) => {
+		console.log(w.length);
+		console.log(this.minFiltered);
+		if (/(.)\1{1,}/.test(w) && w.length > this.minFiltered)
+			//only tests those with at least one double char
+			return allPossibleCases(combos(charSplitter(w)));
+		return w;
+	});
 }
 function charSplitter(word) {
 	let arr = [];
@@ -56,8 +154,7 @@ function allPossibleCases(arr) {
 	var result = [];
 	var allCasesOfRest = allPossibleCases(arr.slice(1)); // recur with the rest of array
 	for (var i = 0; i < allCasesOfRest.length; i++)
-		for (var j = 0; j < arr[0].length; j++) 
-			result.push(arr[0][j] + allCasesOfRest[i]);
+		for (var j = 0; j < arr[0].length; j++) result.push(arr[0][j] + allCasesOfRest[i]);
 	return result;
 }
 function combos(val) {
--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 const Filter = require("./index.js");
 const filter = new Filter();
-console.time("filter");
-filter.debug("@no, im not going to friend you you level up like 3 times faster than me\n@Nate | etaN na dur ur a higher lvl than me");
-console.timeEnd("filter");
+// console.time("filter");
+// filter.debug("@no, im not going to friend you you level up like 3 times faster than me\n@Nate | etaN na dur ur a higher lvl than me");
+// console.timeEnd("filter");
 
-console.time("filter");
-filter.debug(`@tirsdag (tiril) we've been communicating. I don't see anything wrong with the server. Also as for the whole "quality over quantity" thing, any social media, or community, attracts much more quality members if they have the quantity. Sometimes you need quantity to gain quality. We have some quality already though and it's strange to see some of you still think there's something "off". If something is off to you, try to get the original people you hung around active again, or accept the fact that people come and go. You can't expect everyone to always stay. Also, I deleted the messages because they were encouraging others to leave. They would do more harm staying there than if we deleted. I don't care if a few dumbasses come out and say "hey, you deleted that this is bad server", it's  much better than a quality member looking at it and wanting to leave.`);
-console.timeEnd("filter");
+// console.time("filter");
+// filter.debug(`@tirsdag (tiril) we've been communicating. I don't see anything wrong with the server. Also as for the whole "quality over quantity" thing, any social media, or community, attracts much more quality members if they have the quantity. Sometimes you need quantity to gain quality. We have some quality already though and it's strange to see some of you still think there's something "off". If something is off to you, try to get the original people you hung around active again, or accept the fact that people come and go. You can't expect everyone to always stay. Also, I deleted the messages because they were encouraging others to leave. They would do more harm staying there than if we deleted. I don't care if a few dumbasses come out and say "hey, you deleted that this is bad server", it's  much better than a quality member looking at it and wanting to leave.`);
+// console.timeEnd("filter");
+// console.log(filter.debug());
+// console.log(filter.getAllCombos("corn"))
+console.log(filter.clean("this is porn and always censored because niggers"));
+console.log(filter.isUnclean("porn"));
